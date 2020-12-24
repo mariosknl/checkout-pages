c1cdd7190109c58f5aec84e741f91c3394b38f20
mariosknl <mariosknl@outlook.com>
2020-12-24 18:09:09 +0200

    creating the layout for the order form

--- src/components/Order Summary/OrderSummary.js
+++ src/components/Order Summary/OrderSummary.js
@@ -4,6 +4,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { addProduct, removeProduct } from "../../reducers/cartSlice";
 import { getPrice } from "./getPrice";
 
+import {
+  ButtonStyles,
+  CartContainerStyles,
+  CartInnerContainerStyles,
+  ListStyles,
+} from "../styles/OrderSummaryStyles";
+
 const OrderSummary = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.cartInfo.products);
@@ -17,67 +24,56 @@ const OrderSummary = () => {
   const displayedProducts = [...products];
 
   return (
-    <>
-      <h1>Order Summary</h1>
-      {displayedProducts.map((item) => {
-        if (displayedProducts.includes(item.id)) {
-          return "";
-        } else {
-          displayedProducts.push(item.id);
+    <div>
+      <h2>Order Summary</h2>
+      <CartContainerStyles>
+        {displayedProducts.map((item) => {
+          if (displayedProducts.includes(item.id)) {
+            return "";
+          } else {
+            displayedProducts.push(item.id);
 
-          return (
-            <div key={item.id}>
-              <h2>{item.name}</h2>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <button onClick={() => handleDecrement(item)}>-</button>
-                <p style={{ backgroundColor: "#ccc" }}>{totalProducts}</p>
-                <button onClick={() => handleIncrement(item)}>+</button>
-              </div>
-              <img src={item.img} alt={item.name} />
-              <ul
-                style={{
-                  width: "100%",
-                  display: "grid",
-                  gridTemplateColumns: "1fr 1fr",
-                  listStyleType: "none",
-                }}
-              >
-                <li>{item.description}</li>
-                <li>&#8364;{totalPrice}</li>
-                <li>Shipping</li>
-                <li>&#8364;0</li>
-                <li>Discount {item.discount}%</li>
-                <li>
-                  {item.price -
-                    Number(getPrice(item.price, totalDiscount)).toFixed(0)}
-                </li>
-              </ul>
-              <hr />
-              <ul
-                style={{
-                  display: "grid",
-                  gridTemplateColumns: "1fr 1fr",
-                  listStyleType: "none",
-                }}
-              >
-                <li>Total</li>
-                <li>
-                  {totalPrice -
-                    item.price +
-                    Number(getPrice(item.price, totalDiscount).toFixed(0))}
-                </li>
-              </ul>
-            </div>
-          );
-        }
-      })}
-    </>
+            return (
+              <CartInnerContainerStyles key={item.id}>
+                <ButtonStyles>
+                  <h2>{item.name}</h2>
+                  <div className="buttons">
+                    <button onClick={() => handleDecrement(item)} type="button">
+                      -
+                    </button>
+                    <p>{totalProducts}</p>
+                    <button onClick={() => handleIncrement(item)} type="button">
+                      +
+                    </button>
+                  </div>
+                </ButtonStyles>
+                <img src={item.img} alt={item.name} />
+                <ListStyles>
+                  <li>{item.description}</li>
+                  <li>&#8364;{totalPrice}</li>
+                  <li>Shipping</li>
+                  <li>&#8364;0</li>
+                  <li>Discount {item.discount}%</li>
+                  <li>
+                    {item.price -
+                      Number(getPrice(item.price, totalDiscount)).toFixed(0)}
+                  </li>
+                </ListStyles>
+                <hr />
+                <ListStyles>
+                  <li>Total</li>
+                  <li>
+                    {totalPrice -
+                      item.price +
+                      Number(getPrice(item.price, totalDiscount).toFixed(0))}
+                  </li>
+                </ListStyles>
+              </CartInnerContainerStyles>
+            );
+          }
+        })}
+      </CartContainerStyles>
+    </div>
   );
 };
 
---
+++ src/components/styles/OrderSummaryStyles.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+import { blue } from "../../utils/colors";
+const CartContainerStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CartInnerContainerStyles = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+
+  img {
+    width: 40%;
+  }
+`;
+
+const ButtonStyles = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  h2 {
+    color: ${blue[400]}
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    p {
+      background-color: #ccc;
+      cursor: none;
+      font-size: 16px;
+      padding: 9px;
+    }
+
+    button {
+      cursor: pointer;
+      border: none;
+      font-size: 14px;
+      padding: 10px;
+    }
+  }
+  }
+`;
+
+const ListStyles = styled.ul`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  list-style-type: none;
+`;
+
+export {
+  ButtonStyles,
+  CartContainerStyles,
+  CartInnerContainerStyles,
+  ListStyles,
+};
